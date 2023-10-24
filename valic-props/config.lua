b09e2de027b3da1545133983bc487b830330828d
stepanvalic <valicekstepan8@seznam.cz>
2023-10-24 19:44:18 +0200

    vytvořen prop spawner

---
+++ valic-props/config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+Config.Props = {
+    [1] = {     -- Odstraňte tuto rekvizitu a nahraďte ji jinou, která se vám líbí.
+        model = "prop_tool_bench02_ld",
+        coords = vector4(3817.12, 4442.12, 2.81, 8.18),
+    },
+}
---
+++ valic-props/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5' 
+
+author 'stepan_valic'
+description 'valic prop spawner'
+version '1.0.0'
+
+shared_scripts {
+	'config.lua'
+}
+
+client_scripts {
+	'client/main.lua'
+}
+
+lua54 'yes'
